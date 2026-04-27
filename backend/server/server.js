ef922e285386fe3efc5142f7cfafc44a1e348e01
Square Brackets <agrawalakriti2019@gmail.com>
2026-04-27 14:12:41 +0530

    fix wildcard route for render

--- backend/server/server.js
+++ backend/server/server.js
@@ -57,6 +57,6 @@ app.listen(PORT, () => {
 app.use(express.static(path.join(__dirname, "../dist")));
 
 // fallback route (SAFE)
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
